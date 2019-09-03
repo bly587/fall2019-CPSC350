4199258f3a3d032b7a76a8bbbbe22c03714cb371
tmitchell <tmitchell@chapman.edu>
2019-09-03 19:10:28 +0000

    created a new line

---
+++ test.cpp
@@ -0,0 +1,12 @@
+//test by Titan Mitchell
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argr)
+{
+  int a;
+  cout << "Too easy. Let's do simple math." << endl;
+  cout << "Enter an integer: ";
+  cin >> a;
+  cout << "Your number times 2 = " << a * 2 << endl;
+}
